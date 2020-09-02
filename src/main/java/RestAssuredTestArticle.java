26c5622a111206cde99a0e94535d11502ca56dd7
manunia <timoshenkomaria@mail.ru>
2020-09-02 17:16:30 +0300

    проверяем того, кто больше всего фактов написал

--- src/main/java/RestAssuredTestArticle.java
+++ src/main/java/RestAssuredTestArticle.java
@@ -1,23 +1,27 @@
-import io.restassured.path.json.JsonPath;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.*;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 
 
 public class RestAssuredTestArticle {
 
-    @Test
-    public static void getRequestTest() {
+    private static final String URL = "https://cat-fact.herokuapp.com/facts";
 
+    @Test(description = "Check status code 200")
+    public void getRequestTest() {
         Response response =
                 given()
-                    .log().all()
+                .log().all()
                 .when()
-                .get("https://cat-fact.herokuapp.com/facts")
+                .get(URL)
                 .then()
                 .log().all()
                 .statusCode(200)
@@ -25,9 +29,45 @@ public class RestAssuredTestArticle {
                 .response();
         response.getBody().print();
 
+    }
 
-
+    @Test(description = "Checking the number of repetitions")
+    public void getJsonPathTest() {
+        RestAssured.defaultParser = Parser.JSON;
+        Response response = given()
+                .log().all()
+                .headers("Content-Type", ContentType.JSON, "Assept", ContentType.JSON)
+                .when()
+                .get(URL)
+                .then()
+                .contentType(ContentType.JSON).extract().response();
+        //получаем список всех имен
+        List<Map<String, String>> names = response.jsonPath().getList("all.user.name");
+        Map<String,Integer> counters = new HashMap<>();
+        //подсчитываем уникальные значения
+        for (int i = 0; i < names.size(); i++) {
+            String temp = String.valueOf(names.get(i));
+            if (!counters.containsKey(temp)) {
+                counters.put(temp,1);
+            } else {
+                counters.put(temp, counters.get(temp) + 1);
+            }
+        }
+        //сортируем уникальные значения по ключу
+        List<Map.Entry<String,Integer>> valueList = new ArrayList<>(counters.entrySet());
+        Collections.sort(valueList, new Comparator<Map.Entry<String,Integer>>() {
+            @Override
+            public int compare(Map.Entry<String,Integer> o1, Map.Entry<String,Integer> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+        //тот, кто больше всего фактов написал окажется в конце
+        System.out.println(valueList.get(valueList.size() - 1).getKey());
+        //сравниваем того, кто в конце с интересующим значением
+        Assert.assertEquals(valueList.get(valueList.size() - 1).getKey().equals("{first=Kasimir, last=Schulz}"), true);
 
 
     }
+
+
 }
